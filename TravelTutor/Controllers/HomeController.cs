5146088cba3e46e14e7c2e71c83e8c81a8075d9c
Renāte Vidruska <renate.vidruska@if.lv>
2024-08-21 18:15:57 +0300

    Landing page updates

--- TravelTutor/Controllers/HomeController.cs
+++ TravelTutor/Controllers/HomeController.cs
@@ -9,20 +9,36 @@ public class HomeController(TravelDataService travelDataService) : Controller
 {
     public IActionResult Index()
     {
-        var current = travelDataService.GetCurrent();
-        if (current is null)
-        {
-            travelDataService.SetCurrent(new()
-            {
-                Destination = "Egypt",
-                StartDate = new DateTime(2024, 09, 01),
-                EndDate = new DateTime(2024, 09, 24),
-                HasChildren = false,
-                Activities = ["hiking", "diving"],
-            });
-        }
+        //var current = travelDataService.GetCurrent();
+        //if (current is null)
+        //{
+        //    travelDataService.SetCurrent(new()
+        //    {
+        //        Destination = "Egypt",
+        //        StartDate = new DateTime(2024, 09, 01),
+        //        EndDate = new DateTime(2024, 09, 24),
+        //        HasChildren = false,
+        //        Activities = ["hiking", "diving"],
+        //    });
+        //}
 
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult PostData(UserInputViewModel travelDataInput)
+    {
+        var current = travelDataService.GetCurrent();
+        travelDataService.SetCurrent(new()
+        {
+            Destination = travelDataInput.Destination,
+            StartDate = travelDataInput.StartDate,
+            EndDate = travelDataInput.EndDate,
+            HasChildren = false,
+            Activities = travelDataInput.Activities.Split(',').ToList()
+        });
+
+         return RedirectToAction("Index", "Video");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ TravelTutor/Models/UserInputViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelTutor.Models
+{
+    public class UserInputViewModel
+    {
+        public string Destination { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+        public string Activities { get; set; }
+        public string Purpose { get; set; }
+    }
+}
